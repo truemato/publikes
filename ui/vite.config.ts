e837409d623773dc3f13b556bc39464560e689c9
truemato <serveman520@gmail.com>
2025-02-27 01:54:55 +0900

    it worked

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,7 +1,15 @@
+// ui/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  // ここで明示的に root を指定
+  root: __dirname,
   plugins: [react()],
+
+  // 出力先などを調整したい場合
+  build: {
+    // outDir: '../dist', // 親階層に出力する例
+    // rollupOptions: { ... }
+  }
 })
